dd944d0ceb77d4d2c9d1aa3f5f4434df5fe1c051
Ivan Murzak <ivan.d.murzak@gmail.com>
2017-07-07 19:54:35 +0300

    Question 2

---
+++ src/Answer2.java
@@ -0,0 +1,11 @@
+public class Answer2 {
+    public static void main(String[] args) {
+        int size = 5;
+        for(int y = 0; y < size; y++) {
+            for(int x = 0; x < size; x++) {
+                System.out.print("+ ");
+            }
+            System.out.println("");
+        }
+    }
+}
